d0e3d8fb533320275c87c8b79223ecf5306d0292
biopsysocspi <66943156+biopsysocspi@users.noreply.github.com>
2020-08-16 18:25:10 +0200

    string.replace

---
+++ TROLLSOP/repalceVowel/old.js
@@ -0,0 +1,23 @@
+function disemvowel(str) {
+  
+    let vowels = ["a","e","i","o","u"];
+    
+    let  trolls = "TENISAZUTO";
+    
+    for (let i = 0; i < vowels.length; i++ ) {
+      
+      //str.replace(/i/g,"");
+      
+      //data.replace(new RegExp(`[${regex}]`, 'gi'), '')
+      
+      //let vowel = vowels[i];
+      //let regex = new RegExp(`[${vowel}]`, 'gi');
+      //let regex = `[^${vowel}/gi]$`;
+      //trolls = str.replace(new RegExp(regex, 'gi'), '');
+      //trolls = str.replace("/i/gi", '');
+
+      
+    }
+    console.log(trolls);
+    return trolls;
+  }
